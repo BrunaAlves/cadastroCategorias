a63e671d37b164c54233580c2f83c79c51166bd7
Bruna Alves <brunahalves94@gmail.com>
2018-07-24 19:28:48 -0300

    Peparando index e layout

---
+++ resources/views/layout/app.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Cadastro de Produtos</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <linkhref="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+    <div class="container">
+        <main role="main">
+            @hasSection('body')
+                @yield('body')
+            @endif
+        </main>
+    </div>
+
+    <script src="{{ asset('js/app.js') }}" type="text/javascript"></script>
+</body>
+</html>
